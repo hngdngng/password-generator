01b065679122e2da34f28dc96d5efe1a19323d45
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-06-26 23:56:46 -0400

    Updated JS with validated logic. Fixed recursion error. Refactored code

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+//Traverse DOM elements
+var passwordText = document.querySelector("#password");
+
+// Declare variables
+var specChar, numChar, lowCase, uppCase; //undefined at initiation
+var specCharStr = "@%+\\/'!#$^?:,(){}[]~-_."; // Special characters supported for passwords from: https://docs.oracle.com/cd/E11223_01/doc.910/e11197/app_special_char.htm#MCMAD416
+var numStr = "0123456789";
+var uppStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowStr = "abcdefghijklmnopqrstuvwxyz";
 
 // Write password to the #password input
-function writePassword() {
-
-  // Declare variables
-  var specCharStr = "@%+\\/'!#$^?:,(){}[]~-_."; // Special characters supported for passwords from: https://docs.oracle.com/cd/E11223_01/doc.910/e11197/app_special_char.htm#MCMAD416
-  var numStr = "0123456789";
-  var uppStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowStr = "abcdefghijklmnopqrstuvwxyz";
-  var passwordText = document.querySelector("#password");
-  //console.log(passwordText);
-  var passGen = "";
-
+function getUserInput() {
   // Get user input on password length through a prompt
-  var passLength = prompt("How many characters would you like your password to contain? (minimum 8 char max 128 char)");
+  var passLength = prompt("How many characters would you like your password to contain? (minimum 8 char, max 128 char)");
 
   if (passLength >= 8 && passLength <= 128) {
     //if the user selects a valid number, then ask the series of questions below
-    var specChar = confirm("Click OK to confirm you'd like to include special characters.");
-    var numChar = confirm("Click OK to confirm you'd like to include numeric characters.");
-    var lowCase = confirm("Click OK to confirm you'd like to include lowercase characters.");
-    var uppCase = confirm("Click OK to confirm you'd like to include uppercase characters.");
-    generatePassword();
-    //if the user selects an invalid number, perform recursion which runs the function and prompts length question again until it is valid
-  } else { 
+    specChar = confirm("Click OK to confirm you'd like to include special characters.");
+    numChar = confirm("Click OK to confirm you'd like to include numeric characters.");
+    lowCase = confirm("Click OK to confirm you'd like to include lowercase characters.");
+    uppCase = confirm("Click OK to confirm you'd like to include uppercase characters.");
+    passwordText.value = generatePassword(passLength); //assign #password value to the return result of the generatePassword function when you pass the length through
+  } else if (passLength == null) { //if user cancels 'Generate Password' request, alert them to click when ready
+    alert("Click 'Generate Password' when ready.");
+    //if the user selects an invalid number, perform recursion which runs the function again to prompt length question again until it is valid
+  } else {
     alert("Invalid entry.");
-    writePassword();
+    getUserInput();
   }
-
-  function generatePassword() {
-    var password = "";
-    if (specChar == true) {
-      password = password.concat(specCharStr);
-    }
-    if (numChar == true) {
-      password = password.concat(numStr);
-    }
-    if (lowCase == true) {
-      password = password.concat(uppStr);
-    }
-    if (uppCase == true) {
-      password = password.concat(lowStr);
-    }
-    //console.log(password);
-
-    for (var i = 0, n = password.length; i < passLength; i++) {
-      var ind = Math.floor(Math.random() * n);
-      passGen = passGen.concat(password.charAt(ind));
-      console.log(passGen);
-    }
-  }
-  //generate password in the text area
-  passwordText.value = passGen;
-  //console.log(passwordText.value);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword(userPassLength) {
+  var possChar = "";
+  var passGen = "";
+  
+  if (specChar == true) {
+    possChar = possChar.concat(specCharStr);
+  }
+  if (numChar == true) {
+    possChar = possChar.concat(numStr);
+  }
+  if (lowCase == true) {
+    possChar = possChar.concat(uppStr);
+  }
+  if (uppCase == true) {
+    possChar = possChar.concat(lowStr);
+  }
+
+  for (var i = 0; i < userPassLength; i++) {
+    var ind = Math.floor(Math.random() * possChar.length);
+    passGen = passGen.concat(possChar.charAt(ind));
+  }
+
+  return passGen;
+}
+
+// Add event listener to generate button on click
+document.querySelector("#generate").addEventListener("click", getUserInput);
